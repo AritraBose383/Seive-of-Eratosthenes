e05138313d8f4beb8834aa4eaf9b7d9066f9789e
Aritra Bose <64608228+AritraBose383@users.noreply.github.com>
2020-10-01 11:57:43 +0530

    Add files via upload

---
+++ code (6).cpp
@@ -0,0 +1,37 @@
+//seive of Eratosthenes--------
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+void seive(int  n)
+{
+    //an boolean array
+    bool prime[n+1];
+    memset(prime,true,sizeof(prime));
+    
+    for(int p=2 ;p*p<=n;p++)
+    {
+        if (prime[p]==true)
+        {
+            for (int i=p*p;i<=n;i=i+p)
+            {
+                prime[i]=false;
+            }
+        }
+    }
+
+    for(int j=2;j<=n;j++)
+    {
+        if(prime[j]==true)
+        {
+            cout<<j<<endl;
+        }
+    }
+}
+
+int main() {
+  cout<<"ENTER A NUMBER UPTO WHICH YOU WANT TO PRINT PRIME NUMBERS:"<<endl;
+  int n; cin>>n;
+  seive(n);
+	return 0;
+}
